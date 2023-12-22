cf1959fba314f22d1af8b1392ee5dc067b88aace
Dmitry Petrov <Dima.demon.net@mail.ru>
2023-12-22 22:33:13 +0400

    bugfix: Проверка на автотесты 5

--- controllers/cards.js
+++ controllers/cards.js
@@ -22,7 +22,7 @@ const deleteCard = async (req, res) => {
       () => new Error('NotFoundError'),
     );
 
-    return res.status(200).send();
+    return res.status(200).send({ message: 'Карточка успешно удалена.' });
   } catch (error) {
     if (error.message === 'NotFoundError') {
       return res.status(404).send({ message: 'Карточка с указанным _id не найдена.' });
